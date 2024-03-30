005f1bd0673d716d1b8d932f04be02fa1f1cef43
George Gathogo <gathogogeorge11@gmail.com>
2024-03-30 21:14:31 +0300

    Arithewmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+// Generate a random integer between 0 and 40 (inclusive)
+let random = Math.floor(Math.random() * 41); // 41 because Math.random() excludes upper bound
+
+console.log(random); // Output: Random integer between 0 and 40
+let num3 = 14;
+let num4 = 10;
+
+let mod = (num3 % num4);
+console.log(mod); // Output: 4
+// Define an array containing the set of numbers
+let numbers = [5, 10, 20, 15];
+
+// Use Math.max() to find the maximum value in the set
+// The spread operator (...) is used to spread the elements of the array as arguments to Math.max()
+let max = Math.max(...numbers);
+
+// Output the maximum value
+console.log(max); // Output: 20
